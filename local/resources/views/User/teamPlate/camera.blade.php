3d36f7ea3f7fcd8281ea342ebd7dbe915af315bc
KhaNguyenDinh <khand.cae@gmail.com>
2024-03-13 20:08:23 +0700

    vs3

--- local/resources/views/User/teamPlate/camera.blade.php
+++ local/resources/views/User/teamPlate/camera.blade.php
@@ -1,6 +1,6 @@
 
 @foreach ($camera as $key => $value)
 <h2>{{$value['name_camera']}}</h2> <br>
-<iframe src="https://wss.cordyfoodnt.vn/image?name={{$value['name_camera']}}"
+<iframe src="https://wss.live.cae.vn/image?name={{$value['name_camera']}}"
         width="720" height="480" frameborder="0" allowfullscreen></iframe>
 @endforeach
