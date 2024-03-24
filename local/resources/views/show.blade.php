7fe346633aeda1e3e5802dba9fb3c268b9a7875e
KhaNguyenDinh <khand.cae@gmail.com>
2024-03-25 01:11:26 +0700

    2503

--- local/resources/views/show.blade.php
+++ local/resources/views/show.blade.php
@@ -3,12 +3,12 @@
 @section('action')
 
 <div class="row">
-	<div class="col-sm-1">
+	<div class="col-sm-2">
 	@foreach ($nhaMay as $key => $value)
 		<a href="{{URL::to('Admin/show/'.$value->id_nha_may)}}" class="btn btn-default">{{$value->name_nha_may}}</a><br>
 	@endforeach
 	</div>
-	<div class="col-sm-11">
+	<div class="col-sm-10">
 		@if($id_nha_may!=0)
 		<iframe src="{{URL::to('User/'.$id_nha_may.'/0')}}"></iframe>
 		@endif
