e77fbd840ae853bd4cb5b9cf49a7c13e58858bc9
KhaNguyenDinh <khand.cae@gmail.com>
2024-04-15 15:23:10 +0700

    update

--- local/app/Http/Controllers/dulieuTXTController.php
+++ local/app/Http/Controllers/dulieuTXTController.php
@@ -252,7 +252,7 @@ class dulieuTXTController extends Controller
 				} elseif($interval->m > 0) {$connect = "Mất tín hiệu";
 				} elseif($interval->d > 0) {$connect = "Mất tín hiệu";
 				} elseif($interval->h > 0) {$connect = "Mất tín hiệu";
-				} elseif($interval->i > 10) {$connect = "Mất tín hiệu";}
+				} elseif($interval->i > 30) {$connect = "Mất tín hiệu";}
 				if ($connect!=='') {
 					$list_check['load'] = $list_check['connect'] = 1;
 				}
